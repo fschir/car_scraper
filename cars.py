b60a34986398411b7a2704539cbc209cfe510f2f
fschir <sonnensturz@gmail.com>
2018-05-20 12:59:19 -0700

    Initial commit

---
+++ cars.py
@@ -0,0 +1,244 @@
+import hashlib
+
+from pymongo import MongoClient
+from pymongo import errors
+
+class AbstractBaseCar:
+
+    DEBUG = True
+
+    car_details = {
+        "url": "",
+        "name": "",
+        "price": "",
+        "brand": "",
+        "modell": "",
+        "km_driven": "",
+        "zulassung": "",
+        "ps": "",
+        "farbe": "",
+        "getriebe": "",
+        "hubraum": "",
+        "zylinder": "",
+        "kraftstoff": "",
+        "sha1": "",
+        "isInvalid": False,
+    }
+
+    def __get_info__(self, response):
+        """
+        The actual parsing has to done in this function.
+        :param response:
+        :return:
+        """
+        pass
+
+    def __check_if_valid__(self):
+        """
+        Check if all fields have proper values.
+        :return:
+        """
+        pass
+
+    def _createsha1(self):
+
+        """
+        Creates a sha1 hash of this particular car object to avoid duplicates in the dataset.
+        :return: sha1 hexdigest
+        """
+
+        hash = hashlib.sha1()
+        hash_object = (
+                self.car_details["url"] +
+                self.car_details["name"] +
+                self.car_details["brand"] +
+                self.car_details["modell"] +
+                self.car_details["price"] +
+                self.car_details["km_driven"] +
+                self.car_details["zulassung"] +
+                self.car_details["ps"] +
+                self.car_details["farbe"] +
+                self.car_details["getriebe"] +
+                self.car_details["hubraum"] +
+                self.car_details["kraftstoff"]
+        )
+        hash.update(hash_object.encode('utf-8'))
+        return hash.hexdigest()
+
+    def _feeddatabase(self):
+
+        """
+        Feed Data to MongoDB.
+        We feed a shallow copy to MogoDB to avoid that the same _id_ gets assigned to all objects.
+
+        Is it better to create a new connection for each object and store it individually or collect all
+        objects in memory and bulk feed them?
+
+        The sha1 field shall be the unique Index to avoid duplicates. The database was created with:
+        db.cars.createIndex( { "sha1": 1 }, { unique: true, sparse: true } )
+
+        """
+
+        self.__check_if_valid__()
+        if self.car_details["isInvalid"] == False:
+            client = MongoClient('localhost', 27017)
+            db = client.cars
+            try:
+                this_car = db.cars.insert_one(self.car_details.copy()).inserted_id
+            except errors.DuplicateKeyError:
+                print("Could not insert key: {} \nKey already in Database!\n".format(self.car_details["sha1"]))
+            finally:
+                client.close()
+
+    def __init__(self, response):
+
+        self.__get_info__(response)
+        del self
+
+    def _print_debug(self):
+
+        """
+        Some debug info to check if all the values are correctly parsed and sanitized
+        TODO: remove?
+        :return:
+        """
+
+        print("Url : " + self.car_details["url"])
+        print("Name : " + self.car_details["name"])
+        print("Brand : " + self.car_details["brand"])
+        print("Modell : " + self.car_details["modell"])
+        print("Preis : " + self.car_details["price"])
+        print("KM : " + self.car_details["km_driven"])
+        print("Zulassung : " + self.car_details["zulassung"])
+        print("PS : " + self.car_details["ps"])
+        print("Farbe : " + self.car_details["farbe"])
+        print("Getriebe : " + self.car_details["getriebe"])
+        print("Hubraum : " + self.car_details["hubraum"])
+        print("Zylinder : " + self.car_details["zylinder"])
+        print("Kraftstoff : " + self.car_details["kraftstoff"])
+        print("SHA1 : " + str(self.car_details["sha1"]))
+        print("Is_Invalid : " + str(self.car_details["isInvalid"]))
+        print("\n")
+
+
+class AutoscoutCar(AbstractBaseCar):
+
+    def __get_info__(self, response):
+
+        """
+        The parser, Here is where the magic happens. Read and
+        sanitize the results and assign the according values.
+
+        TODO: Scrapy Fields &? ItemPipeline or handle in Class
+
+        After assigning all values they are fed into the database
+
+        :param response: (scrapy request)
+        :return: NIL
+        """
+        try:
+            name_string = str(response.xpath('/html/body/div[1]/main/div[2]/div/div[1]/h1/span[1]').extract())\
+                .split(" ")
+            self.car_details["url"] = response.url
+            self.car_details["brand"] = name_string[2][13:]
+            self.car_details["modell"] = name_string[3][:-10]
+            self.car_details["price"] = \
+                str(response.xpath('/html/body/div[1]/main/div[3]/div[3]/div[1]/div[1]/h2').extract()).split(" ")[1][
+                :-11].replace(".", "")
+            self.car_details["km_driven"] = \
+                str(response.xpath('/html/body/div[1]/main/div[3]/div[3]/div[2]/div/div[1]/span[1]').extract()).split('>')[
+                    1].split(' ')[0].replace(".", "")
+            self.car_details["zulassung"] = \
+                str(response.xpath('/html/body/div[1]/main/div[3]/div[3]/div[2]/div/div[2]/span[1]').extract()).split('>')[
+                    1][
+                :-6]
+            self.car_details["ps"] = \
+                str(response.xpath('/html/body/div[1]/main/div[3]/div[3]/div[2]/div[1]/div[3]/span[2]').extract()).split(
+                    '>')[
+                    1][:-9]
+            self.car_details["farbe"] = str(response.xpath(
+                '/html/body/div[1]/main/div[4]/div/div/div[7]/div[2]/div[1]/div[2]/dl/dd[2]').extract()).split('\\')[1][1::]
+            self.car_details["getriebe"] = str(response.xpath(
+                '/html/body/div[1]/main/div[4]/div/div/div[7]/div[2]/div[1]/div[3]/dl/dd[1]').extract()).split('\\')[1][1::]
+            self.car_details["kraftstoff"] = str(response.xpath(
+                '/html/body/div[1]/main/div[4]/div/div/div[7]/div[2]/div[2]/div/div/div/div[1]/dl/dd[1]').extract()).split(
+                '\\')[1][1::]
+            self._get_dynamic(response)
+            self.car_details["name"] = self._get_name(response)
+            self.car_details["sha1"] = self._createsha1()
+        except:
+            if self.DEBUG:
+                self._print_debug()
+            else:
+                print("Error")
+
+        self._feeddatabase()
+
+    def _get_name(self, response):
+        full_name = self.car_details['brand'] + " " + self.car_details['modell']
+        try:
+            full_name = full_name + response.xpath('/html/body/div[1]/main/div[2]/div/div[1]/h1/span[2]').split('>')[1][:-6]
+        except:
+            pass
+
+        return full_name
+
+    def _get_dynamic(self, response):
+
+        """
+        There are a few xpath which seem to vary across different sites. This function sanitizes the input and assigns
+        the correct value to each field. Both x-paths can either be the Hubraum or the Zylinder.
+
+        The value of Zylinder can not be greater than 10 so a hacky fix.
+
+        :param response: scrapy.response
+        :return:
+        """
+
+        zyl_or_hubraum = str(response.xpath(
+            '/html/body/div[1]/main/div[4]/div/div/div[7]/div[2]/div[1]/div[3]/dl/dd[3]').extract()).split('\\')[1][
+                         1::].split(' ')[0].replace('.', '')
+
+        try:
+            if int(zyl_or_hubraum) > 10:
+                self.car_details["hubraum"] = zyl_or_hubraum
+                tst = str(response.xpath(
+                    '/html/body/div[1]/main/div[4]/div/div/div[7]/div[2]/div[1]/div[3]/dl/dd[2]').extract()).split(
+                    '\\')[1][
+                      1::].split()[0].replace(".", "")
+                if int(tst) > 10:
+                    self.car_details["isInvalid"] = True
+                else:
+                    self.car_details["zylinder"] = tst
+
+            else:
+                self.car_details["zylinder"] = zyl_or_hubraum
+                self.car_details["hubraum"] = str(response.xpath(
+                    '/html/body/div[1]/main/div[4]/div/div/div[7]/div[2]/div[1]/div[3]/dl/dd[2]').extract()).split(
+                    '\\')[1][
+                                              1::].split()[0].replace(".", "")
+        except ValueError:
+            """ 
+            In rare cases the field can be a random string. In such cases we disregard the entire entry. Due to threading
+            issues the check can not be reliably performed here and needs to be repeated before feeding the Database. 
+
+            TODO: Find a fix!
+            """
+
+            self.car_details["isInvalid"] = True
+
+    def __check_if_valid__(self):
+
+        try:
+            if int(self.car_details["zylinder"]) < 10 and int(self.car_details["hubraum"]) > 100:
+                self.car_details["isInvalid"] = False
+            else:
+                self.car_details["isInvalid"] = True
+
+        except ValueError:
+            self.car_details["isInvalid"] = True
+
+        if self.car_details["modell"] == "":
+            self.car_details["isInvalid"] = True
+
+        pass
---
+++ main.py
@@ -0,0 +1,176 @@
+import scrapy
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from cars import AutoscoutCar
+
+
+urls_to_scrape = []
+
+class BroadSpider(scrapy.Spider):
+
+    name = "Base_Spider"
+    base_url = "https://www.autoscout24.de"
+
+    """
+    Autoscount links look like this:  https://www.autoscout24.de/ergebnisse?mmvmk0=9&mmvmd0=1634&mmvco=1&cy=D&powertype=kw&atype=C&ustate=N%2CU&sort=standard&desc=0
+    where mmvmk0 represents the brand and mmvmd represents the model.
+    
+    This scraper scrapes the site wide and collects all links required for the deep scraping. 
+    """
+
+    brands = {
+        'AUDI': 9,
+        'BMW': 13,
+        'FORD': 29,
+        'Mercedes': 47,
+        'Opel': 54,
+        'Renault': 60,
+        'Volkswagen': 74,
+    }
+    Audi_Modelle = {
+        '100': 1619,
+        '200': 1620,
+        '50': 16581,
+        '80': 1622,
+        '90': 1623,
+        'A1': 19083,
+        'A2': 16416,
+        'A3': 1624,
+        'A4': 1626,
+        'A4 Allroad': 20493,
+        'A5': 19047,
+        'A6': 1628,
+        'A6 allroad': 20494,
+        'A7': 19216,
+        'A8': 1629,
+        'Allroad': 16414,
+        'Cabriolet': 1630,
+        'Coupet': 1631,
+        'Q2': 74373,
+        'Q3': 19715,
+        'Q5': 19155,
+        'Q7': 18683,
+        'Q8': 74734,
+        'Quattro': 18000,
+        'R8': 18925,
+        'RS': 15735,
+        'RS Q3': 20385,
+        'RS2': 19728,
+        'RS3': 19729,
+        'RS4': 19730,
+        'RS5': 19731,
+        'RS6': 19732,
+        'RS7': 20295,
+        'RS7 Sportsback': 20317,
+        'S1': 20369,
+        'S2': 2109,
+        'S3': 15637,
+        'S4': 2108,
+        'S5': 19048,
+        'S6': 1633,
+        'S7': 20155,
+        'S8': 15123,
+        'SQ2': 74735,
+        'SQ5': 20164,
+        'SQ7': 74439,
+        'TT': 15627,
+        'TT RS': 20056,
+        'TTS': 20055,
+        'V8': 1634
+    }
+    BMW_Modelle = {
+        '114': 20149,
+        '116': 18480,
+        '118': 18481,
+        '120': 18482,
+        '123': 19078,
+        '125': 19084,
+        '130': 18588,
+        '135': 19079,
+        '140': 74386,
+        '2002': 2163,
+        '214': 20958,
+        '216': 20397,
+        '218': 20327,
+        '220': 20326,
+        '225': 20328,
+        '228': 20481,
+        '230': 74387,
+        '235': 20329,
+        '240': 74388,
+    }
+
+    def _get_links(self):
+
+        """
+        Create our subset for all our Models and Brands.
+        :return:
+        """
+
+        BaseUrl = 'https://www.autoscout24.de/ergebnisse?sort=standard&desc=0&ustate=N%2CU&cy=D&atype=C'
+        Audi = []
+        BMW = []
+
+        for key, value in self.Audi_Modelle.items():
+            _tmp_url = BaseUrl + '&mmvmk0=%s' % str(self.brands["AUDI"]) + '&mmvmd0=%s' % str(value)
+            for x in range(1, 20):
+                _tmp_url = _tmp_url + '&page=%s' % str(x)
+                Audi.append(_tmp_url)
+
+        for key, value in self.BMW_Modelle.items():
+            _tmp_url = BaseUrl + '&mmvmk0=%s' % str(self.brands["BMW"]) + '&mmvmd0=%s' % str(value)
+            for x in range(1, 20):
+                _tmp_url = _tmp_url + '&page=%s' % str(x)
+                BMW.append(_tmp_url)
+
+        return Audi, BMW
+
+    def start_requests(self):
+
+        Audi, BMW = self._get_links()
+        for url in Audi:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+        for url in BMW:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        all_links = response.css('div.cldt-summary-titles').extract()
+        for x in all_links:
+            global urls_to_scrape
+            urls_to_scrape.append(self.base_url + x.split(" ", 4)[3][6:-7])
+
+    def __init__(self):
+        self.start_requests()
+        print("Hallo")
+
+
+class DeepSpider(scrapy.Spider):
+
+    def __init__(self):
+        pass
+
+    def start_requests(self):
+        x = 0
+        for url in urls_to_scrape:
+            yield scrapy.Request(url=url, callback=self.parse)
+            print(x)
+            x = x + 1
+
+    def parse(self, request):
+        AutoscoutCar(request)
+
+
+@defer.inlineCallbacks
+def main():
+    configure_logging()
+    runner = CrawlerRunner()
+    yield runner.crawl(BroadSpider)
+    yield runner.crawl(DeepSpider)
+    reactor.stop()
+
+
+if __name__ == '__main__':
+    main()
+    reactor.run()
